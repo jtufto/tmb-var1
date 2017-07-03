b26e1608b29bf57ff25814ef4b988b05fa3a56bf
Jarle Tufto <jarle.tufto@ntnu.no>
2017-07-03 13:50:31 +0200

    better starting values

--- var1.R
+++ var1.R
@@ -25,22 +25,26 @@ x[1,] <- mvtnorm::rmvnorm(1,sigma=Gamma0)
 for (t in 2:n) 
   x[t,] <- Phi %*% x[t-1,] + t(mvtnorm::rmvnorm(1,sigma=Sigma))
 
+# Check stationary covariance against theoretical value
+var(x)
+Gamma0
 
-y <- x + rnorm(2*n, sd=.1)
+sdy <- .1
+y <- x + rnorm(2*n, sd=sdy)
 #matplot(1:n, y, type="l",col=1)
 
 library(TMB)
 compile("var1.cpp")
 dyn.load(dynlib("var1"))
 data <- list(y=y)
-burnin <- 100
+burnin <- 10
 parameters <- list(
   x = matrix(0,nrow(y)+burnin, 2),
-  logsdy = log(2),
+  logsdy = log(sdy),
   log_sigma = log(sigma),
-  rho = -.5,
+  rho = rho,
   theta = c(0, pi/2), 
-  logit_eigval = c(1, .5)
+  logit_eigval = qlogis((lambda+1)/2)
 )
 obj <- MakeADFun(data,parameters,random="x")
 obj$method="BFGS"
